f50fa92f7b7cf097247601d261b3151c6e616c71
bhoomildayani182 <bhoomildayani182@gmail.com>
2023-03-26 18:22:25 +0530

    Update Live Database

--- authtests/scripts/seedDB.js
+++ authtests/scripts/seedDB.js
@@ -3,5 +3,5 @@ const db = require('../models');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/reacthealthtracker');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://bhoomildayani182:Bhoomil@1234@cluster0.dwaxlxx.mongodb.net/test');
 
